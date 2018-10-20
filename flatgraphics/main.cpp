baef523d8de22b2f0fdea37b5d1e7142fb472477
Clayton Knittel <cknit1999@gmail.com>
2018-10-20 18:34:58 -0500

    Initial Commit

---
+++ flatgraphics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  flatgraphics
+//
+//  Created by Clayton Knittel on 10/20/18.
+//  Copyright © 2018 Clayton Knittel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
